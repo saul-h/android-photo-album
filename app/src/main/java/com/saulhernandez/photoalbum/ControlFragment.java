01343983bf13f7485ff67d30b5418ebca19c04e2
Chanpheakdey Chum <chanpheakdey.chum@student.csulb.edu>
2021-06-15 22:29:34 -0700

    Created button in the ControlFragment

--- app/src/main/java/com/saulhernandez/photoalbum/ControlFragment.java
+++ app/src/main/java/com/saulhernandez/photoalbum/ControlFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,8 @@ public class ControlFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    View view;
+    public Button btnNext;
 
     public ControlFragment() {
         // Required empty public constructor
@@ -58,7 +62,16 @@ public class ControlFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false);
+        view = inflater.inflate(R.layout.fragment_control, container, false);
+        btnNext = (Button) view.findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/saulhernandez/photoalbum/MainActivity.java
+++ app/src/main/java/com/saulhernandez/photoalbum/MainActivity.java
@@ -1,6 +1,8 @@
 package com.saulhernandez.photoalbum;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment controlFragment = ControlFragment.newInstance("Controller","ControlFragment");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.addToBackStack(null);
+        transaction.commit();
+
     }
 }
