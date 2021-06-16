7c1e5ce1c34d4cdffbed2c02a73fbe65e4d77180
saul-h <saulhernandez217@gmail.com>
2021-06-15 22:55:05 -0700

    Picture fragment where you pass in the ID of a drawable resource that will be displayed in the fragment

--- app/src/main/java/com/saulhernandez/photoalbum/PictureFragment.java
+++ app/src/main/java/com/saulhernandez/photoalbum/PictureFragment.java
@@ -1,12 +1,12 @@
 package com.saulhernandez.photoalbum;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +15,10 @@ import android.view.ViewGroup;
  */
 public class PictureFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_IMAGE_ID = "imageID";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int mImageID;
 
     public PictureFragment() {
         // Required empty public constructor
@@ -32,16 +28,13 @@ public class PictureFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param imageID id of image resource.
      * @return A new instance of fragment PictureFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static PictureFragment newInstance(String param1, String param2) {
+    public static PictureFragment newInstance(int imageID) {
         PictureFragment fragment = new PictureFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_IMAGE_ID, imageID);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +43,7 @@ public class PictureFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mImageID = getArguments().getInt(ARG_IMAGE_ID);
         }
     }
 
@@ -59,6 +51,9 @@ public class PictureFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_picture, container, false);
+        View view = inflater.inflate(R.layout.fragment_picture, container, false);
+        ImageView imageView = view.findViewById(R.id.fragmentContainerPhoto);
+        imageView.setImageResource(mImageID);
+        return view;
     }
 }
